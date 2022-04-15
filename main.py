bcfb8413499e04dbea0a06e00fc22b78368d0ae0
Felix Schneider <fs@felix-schneider.org>
2022-04-15 11:39:18 +0200

    {OTHER} Zeilenlänge auf 120 Zeichen angepasst
    Signed-off-by: Felix Schneider <fs@felix-schneider.org>

--- main.py
+++ main.py
@@ -19,14 +19,13 @@ ABWEICHUNG = 80
 def time_convert(elapsedSeconds):
     minutes = (elapsedSeconds // 60) % 60
     seconds = elapsedSeconds % 60
-    print("--> Fertig nach {} Minuten und {} Sekunden".format(
-        int(minutes), int(seconds)))
+    print("--> Fertig nach {} Minuten und {} Sekunden".format(int(minutes), int(seconds)))
 
 
 def checkIfFileExists(inputPath):
     fileExists = exists(inputPath)
     if not fileExists:
-        print("Die Datei \"" + str(inputPath) + "\" existiert nicht!")
+        print('Die Datei "' + str(inputPath) + '" existiert nicht!')
     return fileExists
 
 
@@ -41,7 +40,7 @@ def getColorToPixel(image, pixel):
 def compareSizesOfImages(image1, image2):
     # Prüfen, ob die Größen der Bilder vielfache voneinander sind
     skaling_faktor = image1.width / image2.width
-    if (image1.height == image2.height * skaling_faktor):
+    if image1.height == image2.height * skaling_faktor:
         return skaling_faktor
 
     return 0
@@ -51,8 +50,7 @@ def compareColorsOfImages(image1, image2, skaling_faktor):
     for x in range(0, image1.width, SCHRITT_GROESSE):  # Vergleich auf x
         for y in range(0, image1.height, SCHRITT_GROESSE):  # Vergleich auf y
             image1R, image1G, image1B = getColorToPixel(image1, (x, y))
-            image2R, image2G, image2B = getColorToPixel(
-                image2, (x / skaling_faktor, y / skaling_faktor))
+            image2R, image2G, image2B = getColorToPixel(image2, (x / skaling_faktor, y / skaling_faktor))
             rotInAbweichung = image1R - ABWEICHUNG <= image2R <= image1R + ABWEICHUNG
             gruenInAbweichung = image1G - ABWEICHUNG <= image2G <= image1G + ABWEICHUNG
             blauInAbweichung = image1B - ABWEICHUNG <= image2B <= image1B + ABWEICHUNG
@@ -65,10 +63,12 @@ def compareColorsOfImages(image1, image2, skaling_faktor):
 def printDuplicateImages(bild1, bild2):
     image1DiskSpace = getsize(bild1)
     image2DiskSpace = getsize(bild2)
-    print((" - {} ({}KB) und {} ({}KB)").format(basename(bild1),
-          image1DiskSpace / 1000, basename(bild2), image2DiskSpace / 1000))
-    print(("       rm \"{}\"\n").format(
-        bild1 if image2DiskSpace > image1DiskSpace else bild2))
+    print(
+        (" - {} ({}KB) und {} ({}KB)").format(
+            basename(bild1), image1DiskSpace / 1000, basename(bild2), image2DiskSpace / 1000
+        )
+    )
+    print(('       rm "{}"\n').format(bild1 if image2DiskSpace > image1DiskSpace else bild2))
 
 
 def compareImages(image1Path, image2Path):
@@ -91,28 +91,26 @@ def compareImages(image1Path, image2Path):
 
 def startComparissonForPath(PFAD):
     alleDateien = glob(PFAD.replace("\\", "/") + "*.jp*g")
-    anzahlKombinationen = sum(
-        1 for _ in combinations(alleDateien, 2))
+    anzahlKombinationen = sum(1 for _ in combinations(alleDateien, 2))
 
     if anzahlKombinationen == 0:
         print("Es wurden keine Bilder im genannten Verzeichnis gefunden!")
         exit(1)
 
-    print("Überprüfe {} Bilder mit {} Kombinationen".format(
-        len(alleDateien), anzahlKombinationen))
+    print("Überprüfe {} Bilder mit {} Kombinationen".format(len(alleDateien), anzahlKombinationen))
 
     startTime = time()
 
-    Parallel(n_jobs=cpu_count())(delayed(compareImages)(
-        kombination[0], kombination[1]) for kombination in combinations(alleDateien, 2))
+    Parallel(n_jobs=cpu_count())(
+        delayed(compareImages)(kombination[0], kombination[1]) for kombination in combinations(alleDateien, 2)
+    )
 
     endTime = time()
     time_convert(endTime - startTime)
 
 
 def getAllDirectionsInPath(Pfad):
-    alleOrdner = glob(
-        Pfad + "/*/", recursive=False) if UNTERORDNER_PRUEFEN else glob(Pfad + "/", recursive=False)
+    alleOrdner = glob(Pfad + "/*/", recursive=False) if UNTERORDNER_PRUEFEN else glob(Pfad + "/", recursive=False)
 
     for ordner in alleOrdner:
         startComparissonForPath(ordner)
@@ -121,7 +119,7 @@ def getAllDirectionsInPath(Pfad):
 if __name__ == "__main__":
     path = input("Pfad, der geprüft werden soll: ")
     if not exists(path):
-        print("\nDas Verzeichnis \"" + str(path) + "\" existiert nicht!")
+        print('\nDas Verzeichnis "' + str(path) + '" existiert nicht!')
         exit(1)
 
     getAllDirectionsInPath(path)
